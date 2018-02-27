653d6198e29e424e2bbe9dab0b10919fafd74412
DenilsonMarcondes <denilson.marcondes@hotmail.com>
2018-02-27 16:27:40 -0300

    tela contato
    foi feito alterações na tela de contato

---
+++ views/contato.php
@@ -0,0 +1,19 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			links
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			coluna 1 
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			coluna 2
+		</div>
+	</div>
+</div>
