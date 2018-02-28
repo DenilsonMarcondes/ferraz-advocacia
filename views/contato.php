63fa74ee0d22d904abb23aa540f2fa962946fce7
DenilsonMarcondes <denilson.marcondes@hotmail.com>
2018-02-28 12:11:58 -0300

    alterações página contato
    foi feito alterações na página contato

--- views/contato.php
+++ views/contato.php
@@ -1,36 +1,82 @@
-<div style="padding: 120px;" class="container">
+<style type="text/css">
+	
+
+	.espaco { padding: 100px;}
+	h3.azul { color: #16197D;}
+	.azul 	{font-weight: bold;}
+	.texto 	{text-align: center; margin: auto;}
+	.botao 	{float: right; background-color: #16197D; padding:6px 12px; height: 40px; width: 80px; color: #fff}
+	.form 	{ border: none; border-bottom: 2px solid #16197D; border-radius: 1px; padding-left: 0px; padding-right: 0px; box-shadow: none;}
+	.form-control{hover: #16197D;}
+    .text   {color:#16197D; }
+    .mapas 	{-webkit-box-sizing: border-box; -moz-box-sizing: border-box;box-sizing: border-box;}
+    .padding {padding: 15px;}
+    
+
+    iframe {
+    border-width: 0px;
+    border-style: inset;
+    border-color: initial;
+    border-image: initial;
+    margin: 10px;
+}
+
+
+.map-responsive{
+    overflow:hidden;
+    padding-bottom:50%;
+    position:relative;
+
+</style>
+
+<div class="container espaco">
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<h4>Matriz Curitiba</h4>
+			<ul class="list-inline texto azul">
+				<li><a href="#">Matriz Curitiba</a></li>
+				<li><a href="#">Filial Maringá</a></li>
+				<li><a href="#">Filial Blumenau</a></li>
+				<li><a href="#">Filial Cascavel</a></li>
+				<li><a href="#">Filial São Paulo</a></li>
+			</ul>
 		</div>
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			Filial Maringá
-		</div>
-	</div>
-</div>
+
 
 <div class="container">
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<b><h3 style="color: #16197D;">MATRIZ CURITIBA</h3></b>
+			<b><h3 class="azul link">MATRIZ CURITIBA</b></h3>
 			<br>
 			<p>Entre em contato por telefone 
 				ou 
 			<p>pelo formulário ao lado:</p>
 			<br>
-			<p><i class="fas fa-map-marker-alt">&nbsp;&nbsp;</i>Rua Alberto Folloni,</p>
+			<p><i class="fas fa-map-marker-alt"></i>Rua Alberto Folloni,</p>
 			<p>541/543, 1o e 2o andares,</p>
 			<p>Juvevê, Curitiba-PR</p>
 			<br>
 			<p><i class="fas fa-phone"></i>Tel.: 41 3595 9200</p>
 		</div>
 
-		<div style="padding: 15px;" class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<h4>Contato</h4>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding">
+			<input type="text"  class="form-control form Roboto Regular"" placeholder="Nome"><br>
+			<input type="text" class="form-control form" placeholder="Email"><br>
+			<input type="text" class="form-control form" placeholder="Telefone"><br>
+			<textarea class="form-control form" rows="10" id="comment" placeholder="Mensagem"></textarea><br>
+			<button type="submit" class="btn btn-formulario size15 botao">ENVIAR</button>
+
 		</div>
+
+	
+
+		
 	</div>
+</div>
+
+
+<div class="fluid">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10193.192062203263!2d-49.266969942545266!3d-25.41024566243187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce41f8586c617%3A0xef8dd8b36a8055e!2sRua+Alberto+Folloni%2C+541+-+Juvev%C3%AA%2C+Curitiba+-+PR!5e0!3m2!1spt-BR!2sbr!4v1511894085947" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
