5f824cb5913f51634e56128b27b5ccd8af483aa5
SampaioO <sampaio.anything@gmail.com>
2018-02-28 06:25:35 -0300

    Home
    Ajustes realizados no rodapé

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,20 +1,45 @@
-<div class="row">
-	<div class="col-md-6">
-	</div>
-	<br>
-	<br>
-	<div class="container">
-		<br>
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="padding: 10px 10px 10px 0px!important;background-color: #FFF;">
-			<img src="img/ferraz-advocacia.png" class="img-responsive"  style="margin: 0 auto; height: 50px;  ">
+	<style type="text/css">
+		/*
+			=====
+			FONTS
+			=====
+		*/
+		@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700');
+		body { font-family: 'Open Sans', sans-serif !important; }
+		/*
+			======
+			COLORS
+			======
+		*/
+		.bg-purple { background-color: #242367 }
+		.bg-purple-alternate { background-color: #1D1C54 }
+
+		/*
+			================
+			UNIVERSAL STYLES
+			================
+		*/
+		.text-center { display: block; text-align: center; text-align: -webkit-center }
+	</style>
+
+	<div class="bg-purple">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding-top: 43px; padding-bottom: 43px">
+					<center><img src="<?= RAIZSITE ?>/img/ferraz-advocacia.png" class="img-responsive" style="height: 55px"></center>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="container">
-	<div class="col-ld-12 col-ld-12 col-ld-12 col-ld-15 padding-zero" style="padding: 5px 10px 10px 5px!important; background-color: #16197D;">
-		<center><p style="color: #fff;">&copy; 2018 Ferraz | CICARELLI & PASSOLD - TODOS OS DIREITOS RESERVADOS </p></center>
+	<div class="bg-purple-alternate">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center" style="padding-top: 13px; padding-bottom: 13px">
+					<span style="font-size: 10px; color: #f5f5f5; font-weight: 600">2018 FERRAZ | CICARELLI & PASSOLD TODOS OS DIREITOS RESERVADOS</span>
+					<a href="http://www.pepperscriacao.com" style="font-size: 12px; float: right; color: #f5f5f5"><span style="font-weight: 600">pepperscriacao</strong>.com</a>
+				</div>
+			</div>
+		</div>
 	</div>
-	<p  href="pepperscriacao.com.br" style="float: right;">pepperscriacao.com</p>
-</div>
 </body>
 </html>
