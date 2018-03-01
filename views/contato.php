c5c272d6df35f062925c3854f317318e27d6cd1c
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-01 16:18:52 -0300

    Site
    Iniciando ajustes na página de contato

--- views/contato.php
+++ views/contato.php
@@ -5,16 +5,12 @@
 	h3.azul { color: #16197D;}
 	.azul 	{font-weight: bold; color: #16197D;}
 	.texto 	{text-align: center; margin: auto;}
-	.botao 	{float: right; background-color: #16197D; padding:6px 12px; height: 40px; width: 90px; color: #fff}
-	.form 	{ border: none; border-bottom: 2px solid #16197D; border-radius: 1px; padding-left: 0px; padding-right: 0px; box-shadow: none;}
-	.form-control{hover: #16197D;}
     .text   {color:#16197D; }
     .mapas 	{-webkit-box-sizing: border-box; -moz-box-sizing: border-box;box-sizing: border-box;}
     .padding {padding: 15px;}
     .list-inline>li {padding: 40px;}
 	iframe {border-width: 0px; border-style: inset; border-color: initial; border-image: initial; margin: 10px; }
 	.map-responsive{overflow:hidden; padding-bottom:50%; position:relative;}
-   	.form-control:focus {border-color: #16197D; outline: 0; -webkit-box-shadow: none; box-shadow: none;}
 	a { color: #16197D; text-decoration: none; color: bold; }
 
 </style>
@@ -24,7 +20,7 @@
 
 <div class="container espaco">
 	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
 			<ul class="list-inline texto azul">
 				<li><a href="#">Matriz Curitiba</a></li>
 				<li><a href="#">Filial Maringá</a></li>
@@ -36,39 +32,37 @@
 	</div>
 </div>
 
-
 <div class="container">
-	<div class="row">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<b><h3 class="azul link">MATRIZ CURITIBA</b></h3>
-			<br>
-			<p>Entre em contato por telefone 
-				ou 
-			<p>pelo formulário ao lado:</p>
-			<br>
-			<p><i class="fas fa-map-marker-alt"></i>Rua Alberto Folloni,</p>
-			<p>541/543, 1o e 2o andares,</p>
-			<p>Juvevê, Curitiba-PR</p>
-			<br>
-			<p><i class="fas fa-phone"></i>Tel.: 41 3595 9200</p>
+	<div class="row row-form">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-content">
+			<h3 class="azul link">MATRIZ CURITIBA</h3>
+
+			<div class="call">
+				<p>Entre em contato por telefone ou pelo formulário ao lado:</p>
+			</div>
+
+			<div class="address">
+				<p>Rua Alberto Folloni, 541/543, 1o e 2o andares, Juvevê, Curitiba-PR</p>
+			</div>
+
+			<div class="phones">
+				<p>Tel.: 41 3595 9200</p>
+			</div>
 		</div>
 
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding">
-			<input type="text"  class="form-control form Roboto Regular"" placeholder="Nome"><br>
-			<input type="text" class="form-control form" placeholder="E-mail"><br>
-			<input type="text" class="form-control form" placeholder="Telefone"><br>
-			<textarea class="form-control form" rows="10" id="comment" placeholder="Mensagem"></textarea><br>
-			<button type="submit" class="btn btn-formulario size15 botao">ENVIAR</button>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-form">
+			<form action="" type="post" class="form-horizontal">
+				<input type="text"  class="form-control form" placeholder="Nome">
+				<input type="text" class="form-control form" placeholder="E-mail">
+				<input type="text" class="form-control form" placeholder="Telefone">
+				<textarea class="form-control form" rows="10" placeholder="Mensagem"></textarea>
 
+				<button type="submit" class="btn btn-formulario size15 botao">ENVIAR</button>
+			</form>
 		</div>
-
-
 	</div>
 </div>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10193.192062203263!2d-49.266969942545266!3d-25.41024566243187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce41f8586c617%3A0xef8dd8b36a8055e!2sRua+Alberto+Folloni%2C+541+-+Juvev%C3%AA%2C+Curitiba+-+PR!5e0!3m2!1spt-BR!2sbr!4v1511894085947" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe><br>
+<iframe class="hidden-xs" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10193.192062203263!2d-49.266969942545266!3d-25.41024566243187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce41f8586c617%3A0xef8dd8b36a8055e!2sRua+Alberto+Folloni%2C+541+-+Juvev%C3%AA%2C+Curitiba+-+PR!5e0!3m2!1spt-BR!2sbr!4v1511894085947" width="100%" height="430" frameborder="0" style="border: 0; margin: 0" allowfullscreen></iframe><br>
 
-<img src="img/advogados.png">
-
-
-
+<img src="img/advogados.png" class="hidden-xs">
