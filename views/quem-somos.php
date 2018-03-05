a1c81ffcbd163b73cc81ed4837ab763ae9e72301
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-05 13:49:35 -0300

    Site - Quem somos
    Inclusão da sessão da equipe

--- views/quem-somos.php
+++ views/quem-somos.php
@@ -2,6 +2,8 @@
 	.session-historico .col-tab .tab-title { font-family: 'Roboto', sans-serif; color: #f5f5f5; text-transform: uppercase }
 	.session-historico .col-tab .tab-description { color: #f5f5f5 }
 
+	.session-equipe { background-color: #f5f5f5 }
+
 	@media screen and (min-width: 768px) {
 		.session-chamada h3.session-title { width: 268px; color: #242367 }
 		.session-chamada .row-chamada { margin-top: 45px; margin-bottom: 45px }
@@ -12,6 +14,27 @@
 		.session-historico .col-tab { height: 377px; position: relative; background-color: #242367 }
 		.session-historico .col-tab .tab-title { line-height: 1.2; font-size: 30px; font-weight: 500 }
 		.session-historico .col-tab .tab-description { line-height: 1.79; font-size: 14px }
+
+		.session-historico .col-missao .nav-tabs { float: right; margin-top: -40px; margin-right: 20px; border: none; }
+		.session-historico .col-missao .nav-tabs>li>a { font-size: 14px; color: #b1b1b1; font-weight: 600; text-transform: uppercase }
+		.session-historico .col-missao .nav>li>a { padding: 0; margin-right: 45px; border-bottom: 1px solid #b1b1b1; padding-right: 43px; margin-right: 10px; border-top: none }
+
+		.nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li.active>a:hover { color: #ffffff !important; border-bottom: 1px solid #ffffff !important; background-color: transparent; border-radius: 0; border: none; }
+		.nav>li>a:focus, .nav>li>a:hover { background-color: transparent; border: none; border-radius: 0; }
+
+		.session-equipe { padding-top: 110px; padding-bottom: 110px }
+		.session-equipe .membro:first-child,
+		.session-equipe .membro:nth-child(2),
+		.session-equipe .membro:nth-child(3),
+		.session-equipe .membro:nth-child(5),
+		.session-equipe .membro:nth-child(6) { border-right: 1px solid #242367 }
+		.session-equipe .membro .imagem { height: 264px }
+		.session-equipe .membro .imagem h3 { font-size: 30px; font-family: 'Roboto', sans-serif; line-height: 1.2; color: #242367; margin: 0; font-weight: 500; width: 212px }
+		.session-equipe .membro .imagem p { font-size: 14px; line-height: 1.79; color: #4a4a4a; width: 268px; margin-top: 10px }
+
+		.session-equipe .membro .conteudo h3 { font-size: 20px; font-family: 'Roboto', sans-serif; line-height: 1; color: #242367; text-transform: uppercase; margin: 0; margin-top: 28px }
+		.session-equipe .membro .conteudo span { font-size: 12px; font-family: 'Roboto', sans-serif; line-height: 1.67; color: #242367; font-style: italic; }
+		.session-equipe .membro .conteudo p { font-size: 14px; line-height: 1.79; color: #4a4a4a; margin-bottom: 40px; margin-top: 5px }
 	}
 </style>
 
@@ -35,15 +58,108 @@
 		<div class="line hidden-xs"></div>
 
 		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-historico">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-historico" style="padding-right: 0px">
+				<div style="height: 105px; background-color: #ffffff; margin-top: -105px" class="hidden-xs"></div>
+
 				<p class="bread-crumb-title">o histórico</p>
 				<h3 class="session-title">Ferraz | Cicarelli & Passold Advogados Associados</h3>
 				<p class="session-content">Fundado em 2001, na cidade de Curitiba, Ferraz | Cicarelli & Passold Advogados Associados é um escritório de advocacia que está presente em três estados: Paraná, Santa Catarina e São Paulo. Além disso, conta com parceiros éticos e de confi ança em praticamente todas as regiões do Brasil, sempre priorizando a qualidade dos serviços e resultados aos clientes.</p>
 				<p class="session-content">Apresentando um trabalho sério, eficiente e que supera as expectativas dos clientes, o escritório teve um crescimento rápido sem nunca esquecer dos valores e da excelência em todas as áreas de atuação. O FCP Advogados Associados é referência nacional em serviços jurídicos e de recuperação de ativos e está pronto para atender sua empresa.</p>
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-tab">
-				<p class="tab-title">Missão</p>
-				<p class="tab-description">A missão do escritório é buscar resultado ao cliente, superando expectativas mediante gestão de informação, serviços jurídicos e de recuperação de crédito com qualidade, bem como proporcionar oportunidades aos colaboradores e crescimento da empresa.</p>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-missao" style="padding-left: 0px">
+				<ul class="nav nav-tabs">
+					<li role="presentation" class="active"><a data-toggle="tab" href="#missao">Missão</a></li>
+					<li role="presentation"><a data-toggle="tab" href="#visao" role="tab">Visão</a></li>
+					<li role="presentation"><a data-toggle="tab" href="#valores" role="tab">Valores</a></li>
+				</ul>
+				<div class="col-tab">
+					<div class="tab-content">
+						<div id="missao" class="tab-pane fade in active">
+							<div class="position-center-center">
+								<p class="tab-title">Missão</p>
+								<p class="tab-description">A missão do escritório é buscar resultado ao cliente, superando expectativas mediante gestão de informação, serviços jurídicos e de recuperação de crédito com qualidade, bem como proporcionar oportunidades aos colaboradores e crescimento da empresa.</p>
+							</div>
+						</div>
+
+						<div id="visao" class="tab-pane fade">
+							<div class="position-center-center">
+								<p class="tab-title">Visão</p>
+								<p class="tab-description">A missão do escritório é buscar resultado ao cliente, superando expectativas mediante gestão de informação, serviços jurídicos e de recuperação de crédito com qualidade, bem como proporcionar oportunidades aos colaboradores e crescimento da empresa.</p>
+							</div>
+						</div>
+
+						<div id="valores" class="tab-pane fade">
+							<div class="position-center-center">
+								<p class="tab-title">Valores</p>
+								<p class="tab-description">A missão do escritório é buscar resultado ao cliente, superando expectativas mediante gestão de informação, serviços jurídicos e de recuperação de crédito com qualidade, bem como proporcionar oportunidades aos colaboradores e crescimento da empresa.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div style="background: url('<?= RAIZSITE ?>/img/marcacao-5.jpg'); height: 250px; background-position: center center"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="session-equipe">
+	<div class="container hidden-xs">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs" style="margin-bottom: 27px">
+				<p class="bread-crumb-title">quem somos</p>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem">
+					<h3>O escritório tem como sócios-titulares</h3>
+					<p>advogados especializados em diversas áreas, sendo eles:</p>
+				</div>
+				<div class="conteudo">
+					<h3>Valeria caramuru cicarelli</h3>
+					<span>(in memoriam)</span>
+					<p>Inscrita na OAB/PR sob o n. 25.474, formou-se pela Faculdade de Direito de Curitiba, com especialização em Direito Processual Civil, concluindo o curso em 1997. Cursou a Escola do Ministério Público no ano de 1998.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem" style="background: url('<?= RAIZSITE ?>/img/alexandre.jpg'); height: 264px; background-position: center"></div>
+				<div class="conteudo">
+					<h3>ALEXANDRE NELSON FERRAZ</h3>
+					<span>&nbsp;</span>
+					<p>Inscrito na OAB/PR sob o n. 30.890, na OAB/SC sob o n. 36.530, na OAB/SP sob o n. 382.471 e na OAB/MT sob o n. 22.640/A, formou-se pela FURB - Fundação Universidade Regional de Blumenau.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem"></div>
+				<div class="conteudo">
+					<h3>Valeria caramuru cicarelli</h3>
+					<span>(in memoriam)</span>
+					<p>Inscrita na OAB/PR sob o n. 25.474, formou-se pela Faculdade de Direito de Curitiba, com especialização em Direito Processual Civil, concluindo o curso em 1997. Cursou a Escola do Ministério Público no ano de 1998.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem"></div>
+				<div class="conteudo">
+					<h3>Valeria caramuru cicarelli</h3>
+					<span>(in memoriam)</span>
+					<p>Inscrita na OAB/PR sob o n. 25.474, formou-se pela Faculdade de Direito de Curitiba, com especialização em Direito Processual Civil, concluindo o curso em 1997. Cursou a Escola do Ministério Público no ano de 1998.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem"></div>
+				<div class="conteudo">
+					<h3>Valeria caramuru cicarelli</h3>
+					<span>(in memoriam)</span>
+					<p>Inscrita na OAB/PR sob o n. 25.474, formou-se pela Faculdade de Direito de Curitiba, com especialização em Direito Processual Civil, concluindo o curso em 1997. Cursou a Escola do Ministério Público no ano de 1998.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs membro">
+				<div class="imagem"></div>
+				<div class="conteudo">
+					<h3>Valeria caramuru cicarelli</h3>
+					<span>(in memoriam)</span>
+					<p>Inscrita na OAB/PR sob o n. 25.474, formou-se pela Faculdade de Direito de Curitiba, com especialização em Direito Processual Civil, concluindo o curso em 1997. Cursou a Escola do Ministério Público no ano de 1998.</p>
+				</div>
 			</div>
 		</div>
 	</div>
