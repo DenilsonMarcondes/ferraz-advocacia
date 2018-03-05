2ab8ce9dd0fa1240827e776754ce10f850a186c4
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-05 10:54:33 -0300

    Site - Quem somos
    Iniciando ajustes na página

--- views/quem-somos.php
+++ views/quem-somos.php
@@ -1,74 +1,50 @@
 <style type="text/css">
-	
+	.session-historico .col-tab .tab-title { font-family: 'Roboto', sans-serif; color: #f5f5f5; text-transform: uppercase }
+	.session-historico .col-tab .tab-description { color: #f5f5f5 }
+
+	@media screen and (min-width: 768px) {
+		.session-chamada h3.session-title { width: 268px; color: #242367 }
+		.session-chamada .row-chamada { margin-top: 45px; margin-bottom: 45px }
+
+		.session-historico h3.session-title { width: 268px; color: #242367; margin-top: 21px; margin-bottom: 21px }
+		.session-historico p.session-content { width: 382px; color: #4a4a4a }
+
+		.session-historico .col-tab { height: 377px; position: relative; background-color: #242367 }
+		.session-historico .col-tab .tab-title { line-height: 1.2; font-size: 30px; font-weight: 500 }
+		.session-historico .col-tab .tab-description { line-height: 1.79; font-size: 14px }
+	}
 </style>
 
-<h5>Teste</h5>
-<h5>Teste</h5>
-<h5>Teste</h5>
+<div class="margin-header">&nbsp;</div>
+<div class="session-chamada">
+	<div class="container">
+		<div class="row row-chamada">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<h3 class="session-title">Ferraz | Cicarelli & Passold Advogados Associados</h3>
+			</div>
+	  	</div>
+	</div>
+</div>
 
-<br>
-	<br>
+<div class="container">
+	<div style="height: 406px; background: url('<?= RAIZSITE ?>/img/marcacao-4.jpg'); background-size: cover; background-position: center -50px"></div>
+</div>
 
-<!--inicio do container-->
-<div class="container texto">
-	<div class="row">
-		<div class="session-title" class="col-lg-6 col-md-6 col-sm-6 col-xs-12 texto" style="color: #16197D">
-				<h3><b>Ferraz | Cicarelli &</b></h3>
-				<h3><b>Passold Advogados</b></h3>
-				<h3><b>Associados</b></h3>
-		</div>
-		<div class="" class="col-lg-6 col-md-6 col-sm-6 col-xs-12 texto" style="color: #16197D">
+<div class="session-historico">
+	<div class="container">
+		<div class="line hidden-xs"></div>
+
 		<div class="row">
-				
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-historico">
+				<p class="bread-crumb-title">o histórico</p>
+				<h3 class="session-title">Ferraz | Cicarelli & Passold Advogados Associados</h3>
+				<p class="session-content">Fundado em 2001, na cidade de Curitiba, Ferraz | Cicarelli & Passold Advogados Associados é um escritório de advocacia que está presente em três estados: Paraná, Santa Catarina e São Paulo. Além disso, conta com parceiros éticos e de confi ança em praticamente todas as regiões do Brasil, sempre priorizando a qualidade dos serviços e resultados aos clientes.</p>
+				<p class="session-content">Apresentando um trabalho sério, eficiente e que supera as expectativas dos clientes, o escritório teve um crescimento rápido sem nunca esquecer dos valores e da excelência em todas as áreas de atuação. O FCP Advogados Associados é referência nacional em serviços jurídicos e de recuperação de ativos e está pronto para atender sua empresa.</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-tab">
+				<p class="tab-title">Missão</p>
+				<p class="tab-description">A missão do escritório é buscar resultado ao cliente, superando expectativas mediante gestão de informação, serviços jurídicos e de recuperação de crédito com qualidade, bem como proporcionar oportunidades aos colaboradores e crescimento da empresa.</p>
 			</div>
 		</div>
-  	</div>
-</div>
-<br>
-
-<div class="container">
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-		<img src="img/quem-somos.png">
 	</div>
 </div>
-
-<br>
-	<br>	
-
-<div class="container">
-	<div class="session-title" class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="color: #16197D" >
-		<h3><b>Ferraz | Cicarelli &</b></h3>
-		<h3><b>Passold Advogados</b></h3>
-		<h3><b>Associados</b></h3>
-	</div>
-</div>
-
-<div class="container">
-	<div class="bread-crumb-title class=" col-lg-6 col-md-6 col-sm-6 col-xs-12 " style="color: #A9A9A9">
-		<br>
-		<h5>o histórico</h5>
-		<br>
-		<p class="session-content">Fundado em 2001, na cidade de Curtiba, Ferraz | Cicarelli</p>
-		<p class="session-content">& Passold Advogados Associados é um escritório de
-		<p class="session-content">advocacia que está presente e, três estados: Paraná</p>
-		<p class="session-content">Santa Catarina e São Paulo, Além disso, conta com </p>
-		<p class="session-content">parceiros éticos e de confiança em praticamente todas as</p>
-		<p class="session-content">regiões do Brasil, smepre priorizando a qualidade dos</p>
-		<p class="session-content">serviços e resultados aos clientes.</p>
-		<br>
-			<br>
-		<p class="session-content">Apresentando um trabalho sério, eficiente e que supera as</p>
-		<p class="session-content">expectativas dos clientes, o escritório teve um crescimento</p>
-		<p class="session-content">rápido sem nunca esquecer dos valores e da excelencia</p>
-		<p class="session-content">em todas as áreas de atuação. O FCP Advogados</p>
-		<p class="session-content">Associados é referência nacional em serviços jurídicos e</p>
-		<p class="session-content">de recuperação de ativos e está pronto para atender sua</p>
-		<p class="session-content">empresa.</p>
-
-	</div>
-</div>
-
-
-
-
-
