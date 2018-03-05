86a09a1bfc8b4f160fb3c0502e6898d572fd1da2
SampaioO <sampaio.anything@gmail.com>
2018-03-05 05:53:33 -0300

    Site - Estrutura
    Ajustes realizados na página

--- views/estrutura.php
+++ views/estrutura.php
@@ -1,34 +1,66 @@
 <style type="text/css">
-	
+	/*
+		================
+		ESTRUTURA STYLES
+		================
+	*/
+	.session-conteudo { background-color: #f5f5f5 }
+	.session-conteudo .line { width: 94%; top: 9px }
+
+	.session-socios { background-color: #242367 }
+	@media screen and (min-width: 768px) {
+		.session-conteudo { padding-top: 100px; padding-bottom: 100px }
+		.session-conteudo h3 { margin-top: 37px; width: 268px }
+		.session-conteudo p.session-content-alternative { margin-top: 15px }
+
+		.session-socios p.session-content { margin-top: 40px }
+		.session-socios .col-conteudo { height: 519px; position: relative; }
+		.session-socios .col-conteudo .position-center-vertical { width: 340px }
+	}
+	@media screen and (max-width: 768px) {
+		.session-conteudo { padding-top: 63px; padding-bottom: 63px }
+		.session-conteudo h3 { margin-top: 15px; width: 191px }
+		.session-conteudo p.session-content-alternative { margin-top: 21px }
+
+		.session-socios p.session-content { margin-top: 20px }
+		.session-socios .col-conteudo { height: 441px; position: relative; }
+	}
 </style>
 
-<h5>.</h5>
-<h5>.</h5>
-<h5>.</h5>
-<h5>.</h5>
+<div class="parallax-topo margin-header" style="background: url('<?= RAIZSITE ?>/img/marcacao-2.jpg'); background-position: center center; background-size: cover; background-attachment: fixed">
+	<div class="parallax-filter" style="background: #242367; width: 100%; opacity: 0.5"></div>
+</div>
 
-<div class="container texto">
-	<div class="row">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 texto" style="color: #16197D">
-				<h3 class="session-title"><b>Estrutura</b></h3>
-				<h3 class="session-title"><b>Organizacional</b></h3>
-		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 texto" style="color: #16197D">
+<div class="session-conteudo">
+	<div class="container">
 		<div class="row">
-				<h5 class="bread-crumb-title" style="color: #A9A9A9">nossa estrutura</h5>
-				<h5 class="session-content"><b>A estrutura organizacional do escritório permite a </b></h5>
-				<h5 class="session-content"><b>prestação de serviço personalizado a cada um dos</b></h5>
-				<h5 class="session-content"><b>clientes, bem como às diversas especies de</b></h5>
-				<h5 class="session-content"><b>carteiras de processos/contratos.</b></h5>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="color: #16197D">
+				<div class="line hidden-xs"></div>
+
+				<p class="bread-crumb-title hidden-lg hidden-md hidden-sm" style="color: #A9A9A9">nossos premios</p>
+				<h3 class="session-title">Estrutura Organizacional</h3>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="color: #16197D">
+				<p class="bread-crumb-title hidden-xs" style="color: #A9A9A9">nossa estrutura</p>
+				<p class="session-content-alternative">A estrutura organizacional do escritório permite a prestação de serviço personalizado a cada um dos clientes, bem como às diversas espécies de carteiras de processos/contratos.</p>
+			</div>
+	    </div>
+	</div>
+</div>
+
+<div class="session-socios">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-conteudo">
+				<div class="position-center-vertical" style="color: #ffffff">
+					<p class="session-content-alternative">Os advogados-sócios e seniores atuam como Diretores, capitaneando as equipes, também divididas por clientes e especialidades.</p>
+					<p class="session-content">O conselho diretor é formado pelos sócios, que decidem a estratégia de todo escritório. Cada filial possui um advogado gestor responsável que, juntamente com sua equipe, está preparado para atender todas as demandas dos nossos clientes localmente.</p>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs">&nbsp;</div>
 		</div>
 	</div>
-  </div>
 </div>
-		<br>
-	<br>
-<br>
-
-<img src="img/socios.png" width="100%">
 
 <div class="session-estrutura hidden-xs">
 	<div class="container">
@@ -39,8 +71,12 @@
 			</div>
 		</div>
 		<div class="row row-first">
-			<div class="col-lg-9 col-md-9 col-sm-9">
-				<div class="col-lg-12 col-md-12 col-sm-12" style="background: url('<?= RAIZSITE ?>/img/marcacao-home.jpg'); background-size: cover; height: 192px"></div>
+			<div class="col-lg-9 col-md-9 col-sm-9 hidden-xs" style="padding: 0">
+				<div class="col-lg-12 col-md-12 col-sm-12" style="background: url('<?= RAIZSITE ?>/img/marcacao-3.jpg'); background-size: cover; background-attachment: fixed; background-position: top right; height: 192px; padding: 0">
+					<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs" style="border-right: 1px solid #242367; height: 100%; padding: 0">&nbsp;</div>
+					<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs" style="border-right: 1px solid #242367; height: 100%; padding: 0">&nbsp;</div>
+					<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs" style="border-right: 1px solid #242367; height: 100%; padding: 0">&nbsp;</div>
+				</div>
 			</div>
 			<div class="col-lg-3 col-md-3 col-sm-3">
 				<p class="session-title number active">01</p>
@@ -52,13 +88,17 @@
 					</div>
 					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px; margin-top: 15px">
 						<p class="item-address">Rua Alberto Folloni,<br>541/543, 1o e 2o andares,<br>Juvevê, Curitiba-PR</p>
+					</div>
 
+					<div class="col-lg-1 col-md-1 col-sm-1 padding-zero">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="13px" width="13px" viewBox="0 0 578.106 578.106" style="enable-background:new 0 0 578.106 578.106; margin-top: 4px" xml:space="preserve"><g><g><path style="fill:#010002;" d="M577.83,456.128c1.225,9.385-1.635,17.545-8.568,24.48l-81.396,80.781 c-3.672,4.08-8.465,7.551-14.381,10.404c-5.916,2.857-11.729,4.693-17.439,5.508c-0.408,0-1.635,0.105-3.676,0.309 c-2.037,0.203-4.689,0.307-7.953,0.307c-7.754,0-20.301-1.326-37.641-3.979s-38.555-9.182-63.645-19.584 c-25.096-10.404-53.553-26.012-85.376-46.818c-31.823-20.805-65.688-49.367-101.592-85.68 c-28.56-28.152-52.224-55.08-70.992-80.783c-18.768-25.705-33.864-49.471-45.288-71.299 c-11.425-21.828-19.993-41.616-25.705-59.364S4.59,177.362,2.55,164.51s-2.856-22.95-2.448-30.294 c0.408-7.344,0.612-11.424,0.612-12.24c0.816-5.712,2.652-11.526,5.508-17.442s6.324-10.71,10.404-14.382L98.022,8.756 c5.712-5.712,12.24-8.568,19.584-8.568c5.304,0,9.996,1.53,14.076,4.59s7.548,6.834,10.404,11.322l65.484,124.236 c3.672,6.528,4.692,13.668,3.06,21.42c-1.632,7.752-5.1,14.28-10.404,19.584l-29.988,29.988c-0.816,0.816-1.53,2.142-2.142,3.978 s-0.918,3.366-0.918,4.59c1.632,8.568,5.304,18.36,11.016,29.376c4.896,9.792,12.444,21.726,22.644,35.802 s24.684,30.293,43.452,48.653c18.36,18.77,34.68,33.354,48.96,43.76c14.277,10.4,26.215,18.053,35.803,22.949 c9.588,4.896,16.932,7.854,22.031,8.871l7.648,1.531c0.816,0,2.145-0.307,3.979-0.918c1.836-0.613,3.162-1.326,3.979-2.143 l34.883-35.496c7.348-6.527,15.912-9.791,25.705-9.791c6.938,0,12.443,1.223,16.523,3.672h0.611l118.115,69.768 C571.098,441.238,576.197,447.968,577.83,456.128z"/></g></g></svg>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-phone">Tel.: 41 3595 9200</p>
 					</div>
 				</div>
 			</div>
 		</div>
-
 		<div class="row row-second">
 			<div class="col-lg-3 col-md-3 col-sm-3 col">
 				<p class="session-title number">02</p>
@@ -71,7 +111,12 @@
 					</div>
 					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-address">Rua Santos Dumont,<br>2314, cj. 601,<br>Centro, Maringá-PR</p>
+					</div>
 
+					<div class="col-lg-1 col-md-1 col-sm-1 padding-zero">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="13px" width="13px" viewBox="0 0 578.106 578.106" style="enable-background:new 0 0 578.106 578.106; margin-top: 4px" xml:space="preserve"><g><g><path style="fill:#010002;" d="M577.83,456.128c1.225,9.385-1.635,17.545-8.568,24.48l-81.396,80.781 c-3.672,4.08-8.465,7.551-14.381,10.404c-5.916,2.857-11.729,4.693-17.439,5.508c-0.408,0-1.635,0.105-3.676,0.309 c-2.037,0.203-4.689,0.307-7.953,0.307c-7.754,0-20.301-1.326-37.641-3.979s-38.555-9.182-63.645-19.584 c-25.096-10.404-53.553-26.012-85.376-46.818c-31.823-20.805-65.688-49.367-101.592-85.68 c-28.56-28.152-52.224-55.08-70.992-80.783c-18.768-25.705-33.864-49.471-45.288-71.299 c-11.425-21.828-19.993-41.616-25.705-59.364S4.59,177.362,2.55,164.51s-2.856-22.95-2.448-30.294 c0.408-7.344,0.612-11.424,0.612-12.24c0.816-5.712,2.652-11.526,5.508-17.442s6.324-10.71,10.404-14.382L98.022,8.756 c5.712-5.712,12.24-8.568,19.584-8.568c5.304,0,9.996,1.53,14.076,4.59s7.548,6.834,10.404,11.322l65.484,124.236 c3.672,6.528,4.692,13.668,3.06,21.42c-1.632,7.752-5.1,14.28-10.404,19.584l-29.988,29.988c-0.816,0.816-1.53,2.142-2.142,3.978 s-0.918,3.366-0.918,4.59c1.632,8.568,5.304,18.36,11.016,29.376c4.896,9.792,12.444,21.726,22.644,35.802 s24.684,30.293,43.452,48.653c18.36,18.77,34.68,33.354,48.96,43.76c14.277,10.4,26.215,18.053,35.803,22.949 c9.588,4.896,16.932,7.854,22.031,8.871l7.648,1.531c0.816,0,2.145-0.307,3.979-0.918c1.836-0.613,3.162-1.326,3.979-2.143 l34.883-35.496c7.348-6.527,15.912-9.791,25.705-9.791c6.938,0,12.443,1.223,16.523,3.672h0.611l118.115,69.768 C571.098,441.238,576.197,447.968,577.83,456.128z"/></g></g></svg>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-phone">Tel.: 44 3302 5300</p>
 					</div>
 				</div>
@@ -87,7 +132,12 @@
 					</div>
 					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-address">Rua Rodolfo Freygang, 15,<br>Ed. Itaçu, 4o andar, cj. 401,<br>Centro, Blumenau-SC</p>
+					</div>
 
+					<div class="col-lg-1 col-md-1 col-sm-1 padding-zero">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="13px" width="13px" viewBox="0 0 578.106 578.106" style="enable-background:new 0 0 578.106 578.106; margin-top: 4px" xml:space="preserve"><g><g><path style="fill:#010002;" d="M577.83,456.128c1.225,9.385-1.635,17.545-8.568,24.48l-81.396,80.781 c-3.672,4.08-8.465,7.551-14.381,10.404c-5.916,2.857-11.729,4.693-17.439,5.508c-0.408,0-1.635,0.105-3.676,0.309 c-2.037,0.203-4.689,0.307-7.953,0.307c-7.754,0-20.301-1.326-37.641-3.979s-38.555-9.182-63.645-19.584 c-25.096-10.404-53.553-26.012-85.376-46.818c-31.823-20.805-65.688-49.367-101.592-85.68 c-28.56-28.152-52.224-55.08-70.992-80.783c-18.768-25.705-33.864-49.471-45.288-71.299 c-11.425-21.828-19.993-41.616-25.705-59.364S4.59,177.362,2.55,164.51s-2.856-22.95-2.448-30.294 c0.408-7.344,0.612-11.424,0.612-12.24c0.816-5.712,2.652-11.526,5.508-17.442s6.324-10.71,10.404-14.382L98.022,8.756 c5.712-5.712,12.24-8.568,19.584-8.568c5.304,0,9.996,1.53,14.076,4.59s7.548,6.834,10.404,11.322l65.484,124.236 c3.672,6.528,4.692,13.668,3.06,21.42c-1.632,7.752-5.1,14.28-10.404,19.584l-29.988,29.988c-0.816,0.816-1.53,2.142-2.142,3.978 s-0.918,3.366-0.918,4.59c1.632,8.568,5.304,18.36,11.016,29.376c4.896,9.792,12.444,21.726,22.644,35.802 s24.684,30.293,43.452,48.653c18.36,18.77,34.68,33.354,48.96,43.76c14.277,10.4,26.215,18.053,35.803,22.949 c9.588,4.896,16.932,7.854,22.031,8.871l7.648,1.531c0.816,0,2.145-0.307,3.979-0.918c1.836-0.613,3.162-1.326,3.979-2.143 l34.883-35.496c7.348-6.527,15.912-9.791,25.705-9.791c6.938,0,12.443,1.223,16.523,3.672h0.611l118.115,69.768 C571.098,441.238,576.197,447.968,577.83,456.128z"/></g></g></svg>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-phone">Tel.: 47 2102 7150</p>
 						<p class="item-phone">47 3326 3299</p>
 					</div>
@@ -104,7 +154,12 @@
 					</div>
 					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-address">Avenida Brasil,<br>5964, cj. 114<br>Centro, Cascavel-PR</p>
+					</div>
 
+					<div class="col-lg-1 col-md-1 col-sm-1 padding-zero">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="13px" width="13px" viewBox="0 0 578.106 578.106" style="enable-background:new 0 0 578.106 578.106; margin-top: 4px" xml:space="preserve"><g><g><path style="fill:#010002;" d="M577.83,456.128c1.225,9.385-1.635,17.545-8.568,24.48l-81.396,80.781 c-3.672,4.08-8.465,7.551-14.381,10.404c-5.916,2.857-11.729,4.693-17.439,5.508c-0.408,0-1.635,0.105-3.676,0.309 c-2.037,0.203-4.689,0.307-7.953,0.307c-7.754,0-20.301-1.326-37.641-3.979s-38.555-9.182-63.645-19.584 c-25.096-10.404-53.553-26.012-85.376-46.818c-31.823-20.805-65.688-49.367-101.592-85.68 c-28.56-28.152-52.224-55.08-70.992-80.783c-18.768-25.705-33.864-49.471-45.288-71.299 c-11.425-21.828-19.993-41.616-25.705-59.364S4.59,177.362,2.55,164.51s-2.856-22.95-2.448-30.294 c0.408-7.344,0.612-11.424,0.612-12.24c0.816-5.712,2.652-11.526,5.508-17.442s6.324-10.71,10.404-14.382L98.022,8.756 c5.712-5.712,12.24-8.568,19.584-8.568c5.304,0,9.996,1.53,14.076,4.59s7.548,6.834,10.404,11.322l65.484,124.236 c3.672,6.528,4.692,13.668,3.06,21.42c-1.632,7.752-5.1,14.28-10.404,19.584l-29.988,29.988c-0.816,0.816-1.53,2.142-2.142,3.978 s-0.918,3.366-0.918,4.59c1.632,8.568,5.304,18.36,11.016,29.376c4.896,9.792,12.444,21.726,22.644,35.802 s24.684,30.293,43.452,48.653c18.36,18.77,34.68,33.354,48.96,43.76c14.277,10.4,26.215,18.053,35.803,22.949 c9.588,4.896,16.932,7.854,22.031,8.871l7.648,1.531c0.816,0,2.145-0.307,3.979-0.918c1.836-0.613,3.162-1.326,3.979-2.143 l34.883-35.496c7.348-6.527,15.912-9.791,25.705-9.791c6.938,0,12.443,1.223,16.523,3.672h0.611l118.115,69.768 C571.098,441.238,576.197,447.968,577.83,456.128z"/></g></g></svg>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-phone">Tel.: 45 3304 9200</p>
 					</div>
 				</div>
@@ -120,7 +175,12 @@
 					</div>
 					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-address">Rua Leôncio de Carvalho,<br>306, 1o andar, sala 11,<br>Paraíso, São Paulo-SP</p>
+					</div>
 
+					<div class="col-lg-1 col-md-1 col-sm-1 padding-zero">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="13px" width="13px" viewBox="0 0 578.106 578.106" style="enable-background:new 0 0 578.106 578.106; margin-top: 4px" xml:space="preserve"><g><g><path style="fill:#010002;" d="M577.83,456.128c1.225,9.385-1.635,17.545-8.568,24.48l-81.396,80.781 c-3.672,4.08-8.465,7.551-14.381,10.404c-5.916,2.857-11.729,4.693-17.439,5.508c-0.408,0-1.635,0.105-3.676,0.309 c-2.037,0.203-4.689,0.307-7.953,0.307c-7.754,0-20.301-1.326-37.641-3.979s-38.555-9.182-63.645-19.584 c-25.096-10.404-53.553-26.012-85.376-46.818c-31.823-20.805-65.688-49.367-101.592-85.68 c-28.56-28.152-52.224-55.08-70.992-80.783c-18.768-25.705-33.864-49.471-45.288-71.299 c-11.425-21.828-19.993-41.616-25.705-59.364S4.59,177.362,2.55,164.51s-2.856-22.95-2.448-30.294 c0.408-7.344,0.612-11.424,0.612-12.24c0.816-5.712,2.652-11.526,5.508-17.442s6.324-10.71,10.404-14.382L98.022,8.756 c5.712-5.712,12.24-8.568,19.584-8.568c5.304,0,9.996,1.53,14.076,4.59s7.548,6.834,10.404,11.322l65.484,124.236 c3.672,6.528,4.692,13.668,3.06,21.42c-1.632,7.752-5.1,14.28-10.404,19.584l-29.988,29.988c-0.816,0.816-1.53,2.142-2.142,3.978 s-0.918,3.366-0.918,4.59c1.632,8.568,5.304,18.36,11.016,29.376c4.896,9.792,12.444,21.726,22.644,35.802 s24.684,30.293,43.452,48.653c18.36,18.77,34.68,33.354,48.96,43.76c14.277,10.4,26.215,18.053,35.803,22.949 c9.588,4.896,16.932,7.854,22.031,8.871l7.648,1.531c0.816,0,2.145-0.307,3.979-0.918c1.836-0.613,3.162-1.326,3.979-2.143 l34.883-35.496c7.348-6.527,15.912-9.791,25.705-9.791c6.938,0,12.443,1.223,16.523,3.672h0.611l118.115,69.768 C571.098,441.238,576.197,447.968,577.83,456.128z"/></g></g></svg>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 padding-zero" style="margin-left: 5px">
 						<p class="item-phone">Tel.: 11 3255 6376</p>
 					</div>
 				</div>
@@ -128,10 +188,3 @@
 		</div>
 	</div>
 </div>
-
-
- </b>
-</div>
-
-
-
